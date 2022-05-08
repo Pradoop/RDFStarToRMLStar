0137c5dffdc53e636b9d30ddb4657e85697afaa2
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2022-05-08 18:12:55 +0200

    changed the layout of .csv file. working on parent child relationship now

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ import rdflib
 import pandas as pd
 from pyoxigraph import *
 from rdflib import *
-from difflib import SequenceMatcher
-import urllib.parse
 import csv
 import os
 
 
 def askFile():
-    directory = input("Enter directory:")
-    #print('Directory is: ' + directory)
-    return directory
+    dir = input("Enter directory:")
+    return dir
 
 
 def retrieveTripleData(my_directory):
+    sublist_number = 1
+
+
     # Setup lists for storing in excel
     subject_list = list()
     predicate_list = list()
     object_list = list()
+    my_list = list()
 
     for filename in os.listdir(my_directory):
+        counter = 0
         file = os.path.join(my_directory, filename)
         if os.path.isfile(file) and file.endswith(".ttl"):
             result = list(parse(file, "text/turtle"))
@@ -30,10 +32,8 @@ def retrieveTripleData(my_directory):
             print("Something went wrong, please check the directory and make sure that there are no other files in "
                   "the folder besides .ttl and .nt files")
             return
-        #print(result)
         for triple in result:
-            #print(triple)
-            retrieveValues(triple, subject_list, predicate_list, object_list)
+            retrieveValues(triple, subject_list, predicate_list, object_list, my_list)
 
         #print("subject_list")
         #print(subject_list)
@@ -42,69 +42,87 @@ def retrieveTripleData(my_directory):
         #print("object_list")
         #print(object_list)
 
-        data = subject_list + object_list
-        return data
+
+        my_list = [x for x in my_list if x != []]
+        #my_data = subject_list + object_list
+        return my_list
 
 
-def retrieveValues(my_value, my_subject_list, my_predicate_list, my_object_list):
+def retrieveValues(my_value, my_subject_list, my_predicate_list, my_object_list, my_triple_list):
     # What to do for subject, subject can be a NamedNode, BlankNode or Triple
+    new_list = list()
     my_value_subject = my_value.subject
     if isinstance(my_value_subject, Triple):
-        retrieveValues(my_value_subject, my_subject_list, my_predicate_list, my_object_list)
+        retrieveValues(my_value_subject, my_subject_list, my_predicate_list, my_object_list, my_triple_list)
     else:
         my_value_subject = my_value.subject.value
         # check empty
         if not my_subject_list:
             my_subject_list.append(my_value_subject)
+            new_list.append(my_value_subject)
         # check duplicates
-        if my_value_subject not in my_subject_list:
+        elif my_value_subject not in my_subject_list:
             my_subject_list.append(my_value_subject)
+            new_list.append(my_value_subject)
 
     # What to do for predicate, predicate can only be a NamedNode
     my_value_predicate = my_value.predicate.value
-    my_predicate_list.append(my_value_predicate)
+    if not my_predicate_list:
+        my_predicate_list.append(my_value_predicate)
+    # check duplicates
+    elif my_value_predicate not in my_predicate_list:
+        my_predicate_list.append(my_value_predicate)
 
     # What to do for object, object can be a NamedNode,  BlankNode,  Triple or literal
     my_value_object = my_value.object
     if isinstance(my_value_object, Triple):
-        retrieveValues(my_value_object, my_subject_list, my_predicate_list, my_object_list)
+        retrieveValues(my_value_object, my_subject_list, my_predicate_list, my_object_list, my_triple_list)
     elif isinstance(my_value_object, Literal):
         if isinstance(my_value_object.datatype, NamedNode):
             my_value_object = my_value_object.value
             # check empty
             if not my_object_list:
                 my_object_list.append(my_value_object)
+                new_list.append(my_value_object)
             # check duplicates
-            if my_value_object not in my_object_list:
+            elif my_value_object not in my_object_list:
                 my_object_list.append(my_value_object)
+                new_list.append(my_value_object)
     else:
         my_value_object = my_value_object.value
         # check empty
         if not my_object_list:
             my_object_list.append(my_value_object)
+            new_list.append(my_value_object)
         # check duplicates
-        if my_value_object not in my_object_list:
+        elif my_value_object not in my_object_list:
             my_object_list.append(my_value_object)
+            new_list.append(my_value_object)
+
+    my_triple_list.append(new_list)
 
 
 def populateSpreadsheet(my_data):
-    my_header = []
-    output_file = "reverseEngineering.csv"
+    print("my_data:")
+    print(my_data)
+    output = "reverseEngineering.csv"
 
-    for i in my_data:
-        index = my_data.index(i) + 1
-        my_header.append("c" + str(index))
-
-    #print("my_header")
-    #print(my_header)
-
-    f = open(os.getcwd() + "\\" + output_file, 'w', newline='')
+    f = open(os.getcwd() + "\\" + output, 'w', newline='')
     writer = csv.writer(f)
-    writer.writerow(my_header)
-    writer.writerow(my_data)
+
+    for sublist in my_data:
+        sublist_index = my_data.index(sublist) + 1
+        my_header = []
+        for element in sublist:
+            element_index = sublist.index(element) + 1
+            my_header.append("c" + str(sublist_index) + "-" + str(element_index))
+        writer.writerow(my_header)
+        writer.writerow(sublist)
+
+    #writer.writerow(my_data)
     f.close()
 
-    return output_file
+    return output
 
 
 def createMappings(my_directory, my_output_file):
@@ -128,15 +146,16 @@ def createMappings(my_directory, my_output_file):
     g.namespace_manager.bind('', blank)
 
     df = pd.read_csv(os.getcwd() + "\\" + my_output_file, sep=",")
+    filename, file_extension = os.path.splitext(os.getcwd() + "\\" + my_output_file)
     for index, row in df.iterrows():
         print((df.to_string(index=False)))
-
+    file_ext = Literal(file_extension.upper())
 
     # triplesMap
     g.add((URIRef(blank.TriplesMap), RDF.type, rr.TriplesMap))
     # LogicalSource
     g.add((URIRef(blank.TriplesMap), rml.logicalSource, child))  # creates array for logicalSource
-    g.add((child, rml.referenceFormulation, ql.CSV))  # creates content of array
+    g.add((child, rml.referenceFormulation, file_ext))  # creates content of array TODO: find out how to get "ql:file extension" in here
     g.add((child, rml.source, Literal(my_output_file)))  # creates content of array
 
     # subjectMap
@@ -171,7 +190,7 @@ def createMappings(my_directory, my_output_file):
 
 
 if __name__ == '__main__':
-    dir = askFile()
-    data = retrieveTripleData(dir)
+    directory = askFile()
+    data = retrieveTripleData(directory)
     output_file = populateSpreadsheet(data)
-    createMappings(dir, output_file)
+    #createMappings(dir, output_file)
