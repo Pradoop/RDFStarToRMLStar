1cb8105f7f2b92fbad276b9521e41759ccbc6016
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2022-05-08 13:20:05 +0200

    manually generating everything works, now need to work with vars from .csv and from other file

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import rdflib
+import pandas as pd
 from pyoxigraph import *
+from rdflib import *
+from difflib import SequenceMatcher
+import urllib.parse
 import csv
 import os
 
 
-def parseFile():
+def askFile():
     directory = input("Enter directory:")
-    print('Directory is: ' + directory)
+    #print('Directory is: ' + directory)
+    return directory
 
+
+def retrieveTripleData(my_directory):
     # Setup lists for storing in excel
     subject_list = list()
     predicate_list = list()
     object_list = list()
 
-    for filename in os.listdir(directory):
-        file = os.path.join(directory, filename)
+    for filename in os.listdir(my_directory):
+        file = os.path.join(my_directory, filename)
         if os.path.isfile(file) and file.endswith(".ttl"):
             result = list(parse(file, "text/turtle"))
         elif os.path.isfile(file) and file.endswith(".nt"):
@@ -22,20 +30,20 @@ def parseFile():
             print("Something went wrong, please check the directory and make sure that there are no other files in "
                   "the folder besides .ttl and .nt files")
             return
-        print(result)
+        #print(result)
         for triple in result:
-            print(triple)
+            #print(triple)
             retrieveValues(triple, subject_list, predicate_list, object_list)
 
-        print("subject_list")
-        print(subject_list)
-        print("predicate_list")
-        print(predicate_list)
-        print("object_list")
-        print(object_list)
+        #print("subject_list")
+        #print(subject_list)
+        #print("predicate_list")
+        #print(predicate_list)
+        #print("object_list")
+        #print(object_list)
 
         data = subject_list + object_list
-        populateSpreadsheet(data)
+        return data
 
 
 def retrieveValues(my_value, my_subject_list, my_predicate_list, my_object_list):
@@ -81,22 +89,89 @@ def retrieveValues(my_value, my_subject_list, my_predicate_list, my_object_list)
 
 def populateSpreadsheet(my_data):
     my_header = []
-    print("my_header")
-    print(my_header)
+    output_file = "reverseEngineering.csv"
 
     for i in my_data:
         index = my_data.index(i) + 1
         my_header.append("c" + str(index))
 
-    print("my_header")
-    print(my_header)
+    #print("my_header")
+    #print(my_header)
 
-    f = open(os.getcwd() + "\\reverseEngineering.csv", 'w', newline='')
+    f = open(os.getcwd() + "\\" + output_file, 'w', newline='')
     writer = csv.writer(f)
     writer.writerow(my_header)
     writer.writerow(my_data)
     f.close()
 
+    return output_file
+
+
+def createMappings(my_directory, my_output_file):
+    g = rdflib.Graph()
+
+    rr = rdflib.Namespace('http://www.w3.org/ns/r2rml#')
+    rml = rdflib.Namespace('http://semweb.mmlab.be/ns/rml#')
+    ql = rdflib.Namespace('http://semweb.mmlab.be/ns/ql##')
+    ex = rdflib.Namespace('http://example/')
+    blank = rdflib.Namespace('http://example.org/')
+    child = BNode()
+    child2 = BNode()
+    child3 = BNode()
+    child4 = BNode()
+
+    g.namespace_manager.bind('rr', rr)
+    g.namespace_manager.bind('rml', rml)
+    g.namespace_manager.bind('ql', ql)
+    g.namespace_manager.bind('rdf', RDF)
+    g.namespace_manager.bind('ex', ex)
+    g.namespace_manager.bind('', blank)
+
+    df = pd.read_csv(os.getcwd() + "\\" + my_output_file, sep=",")
+    for index, row in df.iterrows():
+        print((df.to_string(index=False)))
+
+
+    # triplesMap
+    g.add((URIRef(blank.TriplesMap), RDF.type, rr.TriplesMap))
+    # LogicalSource
+    g.add((URIRef(blank.TriplesMap), rml.logicalSource, child))  # creates array for logicalSource
+    g.add((child, rml.referenceFormulation, ql.CSV))  # creates content of array
+    g.add((child, rml.source, Literal(my_output_file)))  # creates content of array
+
+    # subjectMap
+    g.add((URIRef(blank.TriplesMap), rml.subjectMap, child2))  # creates array for logicalSource
+    g.add((child2, rml.reference, Literal("c1")))  # creates content of array
+    g.add((child2, rml.termType, rr.BlankNode))  # creates content of array
+
+    # predicateObjectMap
+    g.add((URIRef(blank.TriplesMap), rr.predicateObjectMap, child3))  # creates array for logicalSource
+    g.add((child3, rr.predicate, ex.p))  # creates content of array
+    g.add((child3, rml.objectMap, child4))  # creates content of array
+    g.add((child4, rr.template, Literal("http:example/{c2}")))  # creates content of array
+
+    # for filename in os.listdir(my_directory):
+    #    file = os.path.join(my_directory, filename)
+    #    if os.path.isfile(file) and file.endswith(".ttl"):
+    #        result = list(parse(file, "text/turtle"))
+    #    elif os.path.isfile(file) and file.endswith(".nt"):
+    #        result = list(parse(file, "application/n-triples"))
+    #    else:
+    #        print("Something went wrong, please check the directory and make sure that there are no other files in "
+    #              "the folder besides .ttl and .nt files")
+    #        return
+    #    print(result)
+
+    # for index, row in df.iterrows():
+    #    g.add((URIRef(ex + row['c1']), RDF.type, Literal(row['c1'], datatype=XSD.string)))
+    #    g.add((URIRef(ex + row['c2']), RDF.type, Literal(row['c2'], datatype=XSD.string)))
+    #    g.add((URIRef(ex + row['c3']), RDF.type, Literal(row['c3'], datatype=XSD.string)))
+
+    g.serialize(os.getcwd() + '\\mycsv2rdf.ttl', format='turtle')
+
 
 if __name__ == '__main__':
-    parseFile()
+    dir = askFile()
+    data = retrieveTripleData(dir)
+    output_file = populateSpreadsheet(data)
+    createMappings(dir, output_file)
